a5d2a5f97fe63ae825e456606e09396f40712ca4
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-20 17:33:25 +0200

    tasks added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,34 @@ console.log('JS OK')
 
 //Vue RESET
 console.log('Vue OK', Vue);
+
+const app = Vue.createApp({
+    data() {
+        return {
+            tasks: [
+                {
+                  text: 'Fare colloquio per Amazon',
+                  done: '',
+                },
+                {
+                    text: 'Fare colloquio per Decathlon',
+                    done: '',
+                },
+                {
+                    text: 'Fare colloquio per Coca Cola',
+                    done: '',
+                },
+                {
+                    text: 'Fare colloquio per Netflix',
+                    done: ' ',
+                },
+                {
+                    text: 'Fare colloquio per Meta',
+                    done: ' ',
+                },
+              ], 
+        }
+    }
+});
+    
+app.mount('#root');
